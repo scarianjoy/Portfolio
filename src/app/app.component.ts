255dcf0d074157479a065b7fb4abe1e772d22c3f
Scaria N Joy <scaria1njoy99@gmail.com>
2025-03-02 21:50:44 +0530

    Ng Initial Commt

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'Portfolio';
+  content = 'Content';
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  title = 'Scaria N Joy';
+  subtitle = '.NET | Angular | SQL';
+  content = `Highly skilled and dedicated .NET Core Developer with extensive experience in developing and
+maintaining applications, particularly in the realm of maintenance projects. Known for delivering
+projects on time and exceeding expectations through meticulous attention to detail and a commitment
+to quality`;
+}
